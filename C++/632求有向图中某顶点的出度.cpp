29d07599fa50901b8de3a6414288397fdd79379f
AbelChe <305790018@qq.com>
2018-11-30 11:38:33 +0800

    c++

---
+++ C++/632求有向图中某顶点的出度.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+const int MaxSize = 10;
+struct ArcNode{
+	int adjvex;
+	ArcNode *next;
+}; 
+struct VertexNode{
+	char vertex;
+	ArcNode *firstedge;
+};
+class ALGraph{
+	public:
+		ALGraph(char a[], int n, int e);
+		~ALGraph() {};
+		void display(int v); 
+	private:
+		VertexNode adjlist[MaxSize];
+		int vertexNum, arcNum;
+		int visited0[MaxSize], visited1[MaxSize];
+};
+ALGraph::ALGraph(char a[], int n, int e){
+	int x,y;
+	vertexNum = n;
+	arcNum = e;
+	for(int i = 0;i < vertexNum;i++){
+		visited0[i] = 0;
+		visited1[i] = 0;
+	}
+	for(int i = 0;i < vertexNum;i++){
+		adjlist[i].vertex = a[i];
+		adjlist[i].firstedge = NULL;
+	}
+	for(int i = 0;i < arcNum;i++){
+		cin>>x>>y;
+		ArcNode *s = new ArcNode();
+		s->next = NULL;
+		s->adjvex = y;
+		s->next = adjlist[x].firstedge;
+		adjlist[x].firstedge = s;
+	}
+}
+void ALGraph::display(int v){
+	ArcNode * p;
+	int count = 0;
+	p = adjlist[v].firstedge;
+	while(p != NULL){
+		p = p->next;
+		count++;
+	}
+	cout<<count<<endl;
+}
+int main(){	
+	char a[MaxSize];
+	int n,e,v;
+	cin>>n>>e;
+	for(int i = 0;i < n;i++)
+		cin>>a[i];
+	ALGraph graph(a, n, e);
+	cin>>v;
+	graph.display(v);
+	return 0;
+}
